ffbcdb9cd715fc8c8f49d95cce22773db2ad2865
SugnanaGS <sugnanags@gmail.com>
2025-04-07 00:18:05 +0530

    .

--- demo01/src/main/java/com/mycompany/demo01/Demo01Application.java
+++ demo01/src/main/java/com/mycompany/demo01/Demo01Application.java
@@ -8,6 +8,7 @@ public class Demo01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo01Application.class, args);
+		System.out.println("First changes to push in github");
 	}
 
 }
