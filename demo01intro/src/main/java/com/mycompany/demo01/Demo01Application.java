2bc31dd274a035ec28894e52fabd859355d940eb
SugnanaGS <sugnanags@gmail.com>
2025-04-07 01:00:23 +0530

    .

--- demo01intro/src/main/java/com/mycompany/demo01/Demo01Application.java
+++ demo01intro/src/main/java/com/mycompany/demo01/Demo01Application.java
@@ -8,6 +8,7 @@ public class Demo01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo01Application.class, args);
+		System.out.println("Update for testing");
 	}
 
 }
